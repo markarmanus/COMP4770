cdebcc149ad82f60cd43f3ba89d258aa5ada2973
Brooke Snow <brooke@celtx.com>
2020-04-18 22:51:09 -0230

    start movement system for baby yoda in overworld, add floaty animation to him

--- client/States/OverWorld/Components/MovableC.js
+++ client/States/OverWorld/Components/MovableC.js
@@ -2,10 +2,19 @@ import ComponentTypes from "../../../ComponentTypes";
 import Component from "../../../Component";
 export default class MovableC extends Component {
   constructor({
+    velocity,
+    maxVelocity,
+    accerlationSpeed,
+    friction,
 
   }) {
     super(ComponentTypes.MOVABLE);
     // here you will have all the data that you will
     // need to make baby yoda move from planet to planet
+    this.velocity = velocity;
+    this.maxVelocity = maxVelocity;
+    this.friction = friction;
+    this.currentVelocity = 0;
+    this.accerlationSpeed = accerlationSpeed;
   }
 }
--- client/States/OverWorld/Systems/MovementS.js
+++ client/States/OverWorld/Systems/MovementS.js
@@ -4,10 +4,32 @@ export default class MoementS {
     this.entityManager = entityManager;
   }
 
-  update(planets) {
+  setVelocityWithinBounds(velocity, maxVelocity) {
+    const negativeMax = -1 * maxVelocity;
+    velocity =
+      velocity > maxVelocity
+        ? maxVelocity
+        : velocity < negativeMax
+        ? negativeMax
+        : velocity;
+    return velocity;
+  }
+
+  addFriction(direction, currentVelocity, friction, velocity) {
+    let frictionForce = velocity * friction;
+    frictionForce = direction === "left" ? -1 * frictionForce : frictionForce;
+    let newVelocity = currentVelocity + frictionForce;
+    if (direction === "left" && newVelocity < 0) newVelocity = 0;
+    if (direction === "right" && newVelocity > 0) newVelocity = 0;
+    return newVelocity;
+  }
+
+  //update(planets) {
+  update() {
     const entities = this.entityManager.getEntities();
     for (const entity of entities) {
       const controllsC = entity.components[ComponentTypes.CONTROLABLE];
+      const movementC = entity.components[ComponentTypes.MOVABLE];
       if (controllsC){
       //to access the last mouse location
       console.log(window.mouseTracker.getLocation(0));
@@ -18,9 +40,28 @@ export default class MoementS {
 
       // here you would detect that the user has pressed either (a) to move left
       // or (d) to move right and move him to the correct planet
-      redPlanet = planets[3];
-      const redPlanetRenderC = redPlanet.components[ComponentTypes.RENDERABLE];
-      redPlanetRenderC.posY
+      // redPlanet = planets[3];
+      // const redPlanetRenderC = redPlanet.components[ComponentTypes.RENDERABLE];
+      // redPlanetRenderC.posY
+
+
+        const rightBttn = controllsC.bttnsState[controllsC.rightBttn];
+        const leftBttn = controllsC.bttnsState[controllsC.leftBttn];
+
+        if (rightBttn) {
+          movementC.currentVelocity +=
+            movementC.velocity * movementC.accerlationSpeed;
+        }
+        if (leftBttn) {
+          movementC.currentVelocity -=
+            movementC.velocity * movementC.accerlationSpeed;
+        }
+
+        movementC.currentVelocity = this.setVelocityWithinBounds(
+          movementC.currentVelocity,
+          movementC.maxVelocity
+        );
+
       }
 
     }
